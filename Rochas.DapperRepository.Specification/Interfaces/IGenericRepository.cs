961258da96f1b08f590b86a70143ba98a8c7a6fe
Renato Rocha <rocha2023br@gmail.com>
2025-03-11 13:29:28 -0300

    Added bulk search method to generic repository interface

--- Rochas.DapperRepository.Specification/Interfaces/IGenericRepository.cs
+++ Rochas.DapperRepository.Specification/Interfaces/IGenericRepository.cs
@@ -10,8 +10,8 @@ namespace Rochas.DapperRepository.Specification.Interfaces
         int CountSync(T filterEntity);
         Task<int> Create(T entity, bool persistComposition = false);
         int CreateSync(T entity, bool persistComposition = false);
-        Task CreateRange(IEnumerable<T> entities, bool persistComposition = false);
-        void CreateRangeSync(IEnumerable<T> entities, bool persistComposition = false);
+        Task BulkCreate(IEnumerable<T> entities, bool persistComposition = false);
+        void BulkCreateSync(IEnumerable<T> entities, bool persistComposition = false);
         Task<int> Delete(T filterEntity);
         int DeleteSync(T filterEntity);
         Task<int> Edit(T entity, T filterEntity, bool persistComposition = false);
@@ -22,6 +22,8 @@ namespace Rochas.DapperRepository.Specification.Interfaces
         T GetSync(T filter, bool loadComposition = false);
         Task<ICollection<T>> Search(object criteria, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
         ICollection<T> SearchSync(object criteria, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
+        Task<ICollection<T>> BulkSearch(object[] criterias, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
+        ICollection<T> BulkSearchSync(object[] criterias, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
         Task<ICollection<T>> List(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
         ICollection<T> ListSync(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
 
