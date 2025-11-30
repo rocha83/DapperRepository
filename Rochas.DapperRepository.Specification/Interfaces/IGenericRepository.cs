96736cac16f9816ec29c795d917fd00a0d3e6b90
Renato Rocha <Renato@WINTINYDEV11>
2025-11-30 03:21:56 -0300

    CRUD methods standard specification fix

--- Rochas.DapperRepository.Specification/Interfaces/IGenericRepository.cs
+++ Rochas.DapperRepository.Specification/Interfaces/IGenericRepository.cs
@@ -8,14 +8,14 @@ namespace Rochas.DapperRepository.Specification.Interfaces
         void Initialize(string databaseFileName, string tableScript);
         Task<int> Count(T filterEntity);
         int CountSync(T filterEntity);
-        Task<int> Create(T entity, bool persistComposition = false);
-        int CreateSync(T entity, bool persistComposition = false);
-        Task BulkCreate(IEnumerable<T> entities, bool persistComposition = false);
-        void BulkCreateSync(IEnumerable<T> entities, bool persistComposition = false);
-        Task<int> Delete(T filterEntity);
-        int DeleteSync(T filterEntity);
-        Task<int> Edit(T entity, T filterEntity, bool persistComposition = false);
-        int EditSync(T entity, T filterEntity, bool persistComposition = false);
+        Task<int> Add(T entity, bool persistComposition = false);
+        int AddSync(T entity, bool persistComposition = false);
+        Task BulkAdd(IEnumerable<T> entities, bool persistComposition = false);
+        void BulkAddSync(IEnumerable<T> entities, bool persistComposition = false);
+        Task<int> Remove(T filterEntity);
+        int RemoveSync(T filterEntity);
+        Task<int> Update(T entity, T filterEntity, bool persistComposition = false);
+        int UpdateSync(T entity, T filterEntity, bool persistComposition = false);
         Task<T> Get(object key, bool loadComposition = false);
         T GetSync(object key, bool loadComposition = false);
         Task<T> Get(T filter, bool loadComposition = false);
@@ -24,8 +24,8 @@ namespace Rochas.DapperRepository.Specification.Interfaces
         ICollection<T> SearchSync(object criteria, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
         ICollection<T> BulkSearch(object[] criterias, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
         ICollection<T> BulkSearchSync(object[] criterias, bool loadComposition = false, int recordsLimit = 0, string sortAttributes = null, bool orderDescending = false);
-        Task<ICollection<T>> List(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
-        ICollection<T> ListSync(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
+        Task<ICollection<T>> Query(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
+        ICollection<T> QuerySync(T filter, bool loadComposition = false, int recordsLimit = 0, bool filterConjunction = false, string sortAttributes = null, bool orderDescending = false);
 
         void Dispose();
     }
